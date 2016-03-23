8b5c02190afef94221bfa017800953e06796eda8
Alexey Debelov <aj001@mail.ru>
2016-03-23 14:28:54 +0500

    Исправлен парсинг запроса БС-СМ

--- source/CustomModbusSlave.MicroclimatEs2gApp/BsSm/BuilderBsSmDataCommand32RequestFromCommandPartDataBytes.cs
+++ source/CustomModbusSlave.MicroclimatEs2gApp/BsSm/BuilderBsSmDataCommand32RequestFromCommandPartDataBytes.cs
@@ -22,18 +22,18 @@ namespace CustomModbusSlave.MicroclimatEs2gApp.BsSm {
 
 			// first byte is bytes count
 
-			var temperatureInsideTheCabin = (_commandPartDataBytes[1]*256 + _commandPartDataBytes[2]) - 60;
-			var temperatureOutdoor = (_commandPartDataBytes[3]*256 + _commandPartDataBytes[4]) - 60;
-			var fanSpeed = (_commandPartDataBytes[5] & 0x03);
-			var isTunelModeOn = ((_commandPartDataBytes[5] & 0x04) == 0x04);
-			var isWarmfloorOn = ((_commandPartDataBytes[5] & 0x08) == 0x08);
-			var currentClimaticWorkMode = new ClimaticSystemWorkModeBuilderFromInt(((_commandPartDataBytes[5] & 0xF0) >> 4)).Build();
+			var temperatureInsideTheCabin = _commandPartDataBytes[1] - 60;
+			var temperatureOutdoor = _commandPartDataBytes[2] - 60;
+			var fanSpeed = (_commandPartDataBytes[3] & 0x03);
+			var isTunelModeOn = ((_commandPartDataBytes[3] & 0x04) == 0x04);
+			var isWarmfloorOn = ((_commandPartDataBytes[3] & 0x08) == 0x08);
+			var currentClimaticWorkMode = new ClimaticSystemWorkModeBuilderFromInt(((_commandPartDataBytes[3] & 0xF0) >> 4)).Build();
 
-			var fault1 = (_commandPartDataBytes[6] + _commandPartDataBytes[7] * 256);
-			var fault2 = (_commandPartDataBytes[8] + _commandPartDataBytes[9] * 256);
-			var fault3 = (_commandPartDataBytes[10] + _commandPartDataBytes[11] * 256);
-			var fault4 = (_commandPartDataBytes[12] + _commandPartDataBytes[13] * 256);
-			var fault5 = (_commandPartDataBytes[14] + _commandPartDataBytes[15] * 256);
+			var fault1 = (_commandPartDataBytes[4] + _commandPartDataBytes[5] * 256);
+			var fault2 = (_commandPartDataBytes[6] + _commandPartDataBytes[7] * 256);
+			var fault3 = (_commandPartDataBytes[8] + _commandPartDataBytes[9] * 256);
+			var fault4 = (_commandPartDataBytes[10] + _commandPartDataBytes[11] * 256);
+			var fault5 = (_commandPartDataBytes[12] + _commandPartDataBytes[13] * 256);
 			return  new BsSmDataCommand32RequestSimple(
 				temperatureInsideTheCabin,
 				temperatureOutdoor,
