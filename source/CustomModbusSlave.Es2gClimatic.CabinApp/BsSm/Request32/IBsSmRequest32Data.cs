358284b5afd84e2c7f448a52753d0cbbde8ce91c
Alexey Debelov <aj001@mail.ru>
2018-03-01 16:04:44 +0500

    Code formatting

--- source/CustomModbusSlave.Es2gClimatic.CabinApp/BsSm/Request32/IBsSmRequest32Data.cs
+++ source/CustomModbusSlave.Es2gClimatic.CabinApp/BsSm/Request32/IBsSmRequest32Data.cs
@@ -4,10 +4,12 @@ namespace CustomModbusSlave.Es2gClimatic.CabinApp.BsSm.Request32 {
 	interface IBsSmRequest32Data {
 		int TemperatureInsideTheCabin { get; }
 		int TemperatureOutdoor { get; }
+
 		int FanSpeed { get; }
 		bool IsTunelModeOn { get; }
 		bool IsWarmFloorOn { get; }
 		ClimaticSystemWorkMode CurrentClimaticWorkMode { get; }
+
 		int Fault1 { get; }
 		int Fault2 { get; }
 		int Fault3 { get; }
