6bbbc1af6284e1c9c8abbf115d026042ecb3e1c5
Alexey Debelov <aj001@mail.ru>
2018-05-14 10:47:21 +0500

    Working on InteriorApp, migrating it to shared app skeleton (work in progress)

--- source/CustomModbusSlave.Es2gClimatic.InteriorApp/App.xaml.cs
+++ source/CustomModbusSlave.Es2gClimatic.InteriorApp/App.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Windows;
 using AlienJust.Adaptation.ConsoleLogger;
@@ -11,7 +9,23 @@ using AlienJust.Support.Loggers;
 using AlienJust.Support.Loggers.Contracts;
 using AlienJust.Support.Text;
 using AlienJust.Support.Text.Contracts;
-using AlienJust.Support.UserInterface.Contracts;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.BsSm;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukAirExhauster;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukAirExhauster.Request16;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirRecycle.Reply03;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirRecycle.Request16;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirWinterSummer.Request16;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapOuterAir.Reply03;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapOuterAir.Request16;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapWinterSummer;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFridge;
+using CustomModbusSlave.Es2gClimatic.InteriorApp.SystemDiagnostic;
+using CustomModbusSlave.Es2gClimatic.Shared.AppWindow;
+using CustomModbusSlave.Es2gClimatic.Shared.Bvs;
+using CustomModbusSlave.Es2gClimatic.Shared.MukCondenser.Request16;
+using CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator.Reply03;
+using CustomModbusSlave.Es2gClimatic.Shared.MukFanVaporizer.Request16;
+using CustomModbusSlave.Es2gClimatic.Shared.SetParamsAndKsm;
 using DataAbstractionLevel.Low.PsnConfig;
 using MahApps.Metro;
 
@@ -51,6 +65,71 @@ namespace CustomModbusSlave.Es2gClimatic.InteriorApp
 		}
 		
 		private void App_OnStartup(object sender, StartupEventArgs e) {
+			var appFactory = new AppFactory("psn.Микроклимат-ЭС2ГП-салон.xml");
+			var appAbilities = appFactory.Abilities;
+			
+			// Заслонка наруж. воздуха
+			var cmdListenerMukFlapOuterAirReply03 = new CmdListenerMukFlapOuterAirReply03(2, 3, 47);
+			var cmdListenerMukFlapOuterAirRequest16 = new CmdListenerMukFlapOuterAirRequest16(2, 16, 21);
+
+			// Вентилятор испарителя
+			var cmdListenerMukVaporizerReply03 = new CmdListenerMukVaporizerReply03(3, 3, 41);
+			var cmdListenerMukVaporizerRequest16 = new CmdListenerMukVaporizerRequest16(3, 16, 21);
+
+			// Вентилятор конденсатора
+			var cmdListenerMukCondenserFanReply03 = new CmdListenerMukCondenserFanReply03(4, 3, 29);
+			var cmdListenerMukCondenserRequest16 = new CmdListenerMukCondenserFanRequest16(4, 16, 15);
+
+			// МУК вытяжного вентилятора
+			var cmdListenerMukAirExhausterReply03 = new CmdListenerMukAirExhausterReply03(6, 3, 31);
+			var cmdListenerMukAirExhausterRequest16 = new CmdListenerMukAirExhausterRequest16(6, 16, 21);
+
+			// МУК рециркуляционной заслонки
+			var cmdListenerMukFlapReturnAirReply03 = new CmdListenerMukFlapReturnAirReply03(7, 3, 43);
+			var cmdListenerMukFlapAirRecycleRequest16 = new CmdListenerMukFlapAirRecycleRequest16(7, 16, 21);
+
+			// МУК заслонки зима-лето
+			var cmdListenerMukFlapWinterSummerReply03 = new CmdListenerMukFlapWinterSummerReply03(8, 3, 47);
+			var cmdListenerMukAirFlapWinterSummerRequest16 = new CmdListenerMukFlapAirWinterSummerRequest16(8, 16, 21);
+
+			var cmdListenerBsSmReply32 = new CmdListenerBsSmReply32(10, 32, 47);
+			var cmdListenerBsSmRequest32 = new CmdListenerBsSmRequest32(10, 32, 27);
+
+			var cmdListenerBvs1Reply65 = new CmdListenerBvsReply65(0x1E, 65, 7);
+			var cmdListenerBvs2Reply65 = new CmdListenerBvsReply65(0x1D, 65, 7);
+
+			var cmdListenerKsmParams = new CmdListenerKsmParams(20, 16, 129);
+
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukFlapOuterAirReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukFlapOuterAirRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukVaporizerReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukVaporizerRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukCondenserFanReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukCondenserRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukAirExhausterReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukAirExhausterRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukFlapReturnAirReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukFlapAirRecycleRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukFlapWinterSummerReply03);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerMukAirFlapWinterSummerRequest16);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerBsSmReply32);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerBsSmRequest32);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerBvs1Reply65);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerBvs2Reply65);
+			appAbilities.CmdNotifierStd.AddListener(cmdListenerKsmParams);
+
+			appFactory.ShowMainWindowInOwnThread("Технический абонент, кабина", appAbilities, mainVm => {
+				var channel = mainVm.AddChannel("Single channel");
+				var channel1 = mainVm.AddChannel("Single 2 channel");
+				mainVm.AddTab(new TabItemViewModel {
+					FullHeader = "Диагностика системы",
+					ShortHeader = "ДС",
+					Content = new SystemDiagnosticView()
+				});
+			});
+			
+			
+			
 			_logConsoleDarkRed = new RelayLoggerWithStackTrace(
 				new RelayLogger(new ColoredConsoleLogger(ConsoleColor.DarkRed, ConsoleColor.Black), 
 				new ChainedFormatter(new List<ITextFormatter> {new ThreadFormatter(LogSeporator, true, false, false), new DateTimeFormatter(LogSeporator)})), 
