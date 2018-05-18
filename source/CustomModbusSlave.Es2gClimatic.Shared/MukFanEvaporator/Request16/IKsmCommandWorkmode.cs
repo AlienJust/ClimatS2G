87aca4a460839bee6e90e3aef10a3beb8d4d7eaa
Alexey Debelov <aj001@mail.ru>
2018-05-18 13:12:52 +0500

    Interior: Fixed parameter 3.18 display

--- source/CustomModbusSlave.Es2gClimatic.Shared/MukFanEvaporator/Request16/IKsmCommandWorkmode.cs
+++ source/CustomModbusSlave.Es2gClimatic.Shared/MukFanEvaporator/Request16/IKsmCommandWorkmode.cs
@@ -1,10 +1,10 @@
 ﻿namespace CustomModbusSlave.Es2gClimatic.Shared.MukFanVaporizer.Request16 {
 	public interface IKsmCommandWorkmode {
 		bool AutomaticMode { get; }
-		// zb bit #2 is skipped
 		bool ForceHeatRegulator { get; }
 		bool ForceHeatModePwm100 { get; }
 		bool ForceCoolMode { get; }
+		bool AutomaticModeWithoutSetControl { get; }
 		bool HeatModeAndCoolModeAreOff { get; }
 		bool ManualMode { get; }
 		bool ForceHeatModePwm50 { get; }
