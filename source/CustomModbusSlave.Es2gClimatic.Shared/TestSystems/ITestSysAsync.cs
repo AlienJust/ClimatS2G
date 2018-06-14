dccd21044c8c15beb63ede3ac25651be4d24b78d
Alexey Debelov <aj001@mail.ru>
2018-06-14 08:13:18 +0500

    Started implementation of testing functionality

---
+++ source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/ITestSysAsync.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomModbusSlave.Es2gClimatic.Shared.TestSystems {
+	interface ITestSysAsync
+	{
+		void BeginTest(Action testHasBegun, Action<TestSysResult> testComplete);
+		void CancelCurrentTest();
+	}
+
+	internal enum TestSysResult
+	{
+		Success,
+		Fail,
+		Canceled
+	}
+
+	internal sealed class TestSystemViewModel
+	{
+		private ITestSysAsync
+	}
+}
---
+++ source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/TestSystemView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CustomModbusSlave.Es2gClimatic.Shared.TestSystems {
+	/// <summary>
+	/// Interaction logic for TestSystemControl.xaml
+	/// </summary>
+	public partial class TestSystemView : UserControl {
+		public TestSystemView() {
+			InitializeComponent();
+		}
+	}
+}
