39fc7935a03731768cededa357f9b70b144aaed1
Alexey Debelov <aj001@mail.ru>
2018-06-20 15:12:26 +0500

    Started working on tests

---
+++ source/CustomModbusSlave.Es2gClimatic.InteriorApp/TestSys/Freeze100Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AlienJust.Support.Concurrent.Contracts;
+using CustomModbusSlave.Es2gClimatic.Shared;
+using CustomModbusSlave.Es2gClimatic.Shared.AppWindow;
+using CustomModbusSlave.Es2gClimatic.Shared.TestSystems;
+
+namespace CustomModbusSlave.Es2gClimatic.InteriorApp.TestSys {
+	public class Freeze100Test : ITestSysAsync {
+		private readonly IWorker<Action> _sharedWorker;
+		private bool _isTestCanceled;
+
+		public Freeze100Test(IWorker<Action> sharedWorker, ICmdListenerStd evaporrator03Listener) {
+			_sharedWorker = sharedWorker;
+			_isTestCanceled = false;
+		}
+
+		public void BeginTest(Action testHasBegun, Action<TestSysResult> testComplete) {
+			_sharedWorker.AddWork(() => {
+				testHasBegun.Invoke();
+				var result = TestSysResult.Canceled;
+				
+
+				testComplete.Invoke(result);
+			});
+		}
+
+		public void CancelCurrentTest()
+		{
+			_isTestCanceled = true;
+		}
+	}
+}
--- source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/ITestSysAsync.cs
+++ source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/ITestSysAsync.cs
@@ -1,21 +1,80 @@
 ﻿using System;
+using System.Windows.Input;
+using AlienJust.Support.Concurrent.Contracts;
+using AlienJust.Support.ModelViewViewModel;
 
 namespace CustomModbusSlave.Es2gClimatic.Shared.TestSystems {
-	interface ITestSysAsync
+	public interface ITestSysAsync
 	{
 		void BeginTest(Action testHasBegun, Action<TestSysResult> testComplete);
 		void CancelCurrentTest();
 	}
 
-	internal enum TestSysResult
+	public enum TestSysResult
 	{
 		Success,
 		Fail,
 		Canceled
 	}
 
-	internal sealed class TestSystemViewModel
+	public sealed class TestSystemViewModel :ViewModelBase
 	{
-		private ITestSysAsync
+		private readonly IThreadNotifier _uiNotifier;
+		private readonly DependedCommand _runTestAsync;
+		private readonly DependedCommand _cancelTestAsync;
+		private bool _isRunningTest;
+
+		public ITestSysAsync AssociatedWithModelTest { get; }
+		public string TestName { get; }
+
+		public TestSystemViewModel(IThreadNotifier uiNotifier, string testName, ITestSysAsync asyncTest)
+		{
+			_uiNotifier = uiNotifier;
+			TestName = testName;
+			AssociatedWithModelTest = asyncTest;
+
+
+			_runTestAsync = new DependedCommand(RunTest, ()=>!IsRunningTest);
+			_cancelTestAsync = new DependedCommand(CancelTest, () => IsRunningTest);
+		}
+
+		private void CancelTest()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void RunTest()
+		{
+			AssociatedWithModelTest.BeginTest(() =>
+			{
+				// TODO: test has been really started
+			},
+			testResult=> {
+				switch (testResult)
+				{
+					case TestSysResult.Success:
+						// TODO:
+						break;
+					case TestSysResult.Fail:
+						// TODO:
+						break;
+					case TestSysResult.Canceled:
+						// TODO:
+						break;
+					default:
+						throw new ArgumentOutOfRangeException(nameof(testResult), testResult, null);
+				}
+			});
+		}
+
+		public bool IsRunningTest
+		{
+			get => _isRunningTest;
+			set => SetProp(()=>_isRunningTest != value, ()=> _isRunningTest = value, ()=>IsRunningTest);
+		}
+
+
+		public ICommand RunTestAsync => _runTestAsync;
+		public ICommand CancelTestAsync => _cancelTestAsync;
 	}
 }
