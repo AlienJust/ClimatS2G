0930a5a2b0ce16a2e383e379c62ae620e60a1970
Alexey Debelov <alienjustmail@gmail.com>
2018-08-28 12:36:34 +0500

    Fixed wrong binding of air recycle flap output signals

--- source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/ITestSysAsync.cs
+++ source/CustomModbusSlave.Es2gClimatic.Shared/TestSystems/ITestSysAsync.cs
@@ -4,77 +4,68 @@ using AlienJust.Support.Concurrent.Contracts;
 using AlienJust.Support.ModelViewViewModel;
 
 namespace CustomModbusSlave.Es2gClimatic.Shared.TestSystems {
-	public interface ITestSysAsync
-	{
-		void BeginTest(Action testHasBegun, Action<TestSysResult> testComplete);
-		void CancelCurrentTest();
-	}
+  public interface ITestSysAsync {
+    void BeginTest(Action testHasBegun, Action<TestSysResult> testComplete);
+    void CancelCurrentTest();
+  }
 
-	public enum TestSysResult
-	{
-		Success,
-		Fail,
-		Canceled
-	}
+  public enum TestSysResult {
+    Success,
+    Fail,
+    Canceled
+  }
 
-	public sealed class TestSystemViewModel :ViewModelBase
-	{
-		private readonly IThreadNotifier _uiNotifier;
-		private readonly DependedCommand _runTestAsync;
-		private readonly DependedCommand _cancelTestAsync;
-		private bool _isRunningTest;
+  public sealed class TestSystemViewModel : ViewModelBase {
+    private readonly IThreadNotifier _uiNotifier;
+    private readonly DependedCommand _runTestAsync;
+    private readonly DependedCommand _cancelTestAsync;
+    private bool _isRunningTest;
 
-		public ITestSysAsync AssociatedWithModelTest { get; }
-		public string TestName { get; }
+    public ITestSysAsync AssociatedWithModelTest { get; }
+    public string TestName { get; }
 
-		public TestSystemViewModel(IThreadNotifier uiNotifier, string testName, ITestSysAsync asyncTest)
-		{
-			_uiNotifier = uiNotifier;
-			TestName = testName;
-			AssociatedWithModelTest = asyncTest;
+    public TestSystemViewModel(IThreadNotifier uiNotifier, string testName, ITestSysAsync asyncTest) {
+      _uiNotifier = uiNotifier;
+      TestName = testName;
+      AssociatedWithModelTest = asyncTest;
 
 
-			_runTestAsync = new DependedCommand(RunTest, ()=>!IsRunningTest);
-			_cancelTestAsync = new DependedCommand(CancelTest, () => IsRunningTest);
-		}
+      _runTestAsync = new DependedCommand(RunTest, () => !IsRunningTest);
+      _cancelTestAsync = new DependedCommand(CancelTest, () => IsRunningTest);
+    }
 
-		private void CancelTest()
-		{
-			throw new NotImplementedException();
-		}
+    private void CancelTest() {
+      throw new NotImplementedException();
+    }
 
-		private void RunTest()
-		{
-			AssociatedWithModelTest.BeginTest(() =>
-			{
-				// TODO: test has been really started
-			},
-			testResult=> {
-				switch (testResult)
-				{
-					case TestSysResult.Success:
-						// TODO:
-						break;
-					case TestSysResult.Fail:
-						// TODO:
-						break;
-					case TestSysResult.Canceled:
-						// TODO:
-						break;
-					default:
-						throw new ArgumentOutOfRangeException(nameof(testResult), testResult, null);
-				}
-			});
-		}
+    private void RunTest() {
+      AssociatedWithModelTest.BeginTest(() => {
+        // TODO: test has been really started
+      },
+      testResult => {
+        switch (testResult) {
+          case TestSysResult.Success:
+            // TODO:
+            break;
+          case TestSysResult.Fail:
+            // TODO:
+            break;
+          case TestSysResult.Canceled:
+            // TODO:
+            break;
+          default:
+            throw new ArgumentOutOfRangeException(nameof(testResult), testResult, null);
+        }
+      });
+    }
 
-		public bool IsRunningTest
-		{
-			get => _isRunningTest;
-			set => SetProp(()=>_isRunningTest != value, ()=> _isRunningTest = value, ()=>IsRunningTest);
-		}
+    public bool IsRunningTest {
+      get => _isRunningTest;
+      set => SetProp(() => _isRunningTest != value, () => _isRunningTest = value, () => IsRunningTest);
+    }
 
 
-		public ICommand RunTestAsync => _runTestAsync;
-		public ICommand CancelTestAsync => _cancelTestAsync;
-	}
+    public ICommand RunTestAsync => _runTestAsync;
+    public ICommand CancelTestAsync => _cancelTestAsync;
+  }
 }
