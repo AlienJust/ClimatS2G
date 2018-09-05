3c9e91c2ec9ba8af9ebf10c853f9338c6ce7a51f
Alexey Debelov <alienjustmail@gmail.com>
2018-09-05 15:44:29 +0500

    More code restoration

---
+++ source/CustomModbusSlave.Es2gClimatic.CabinApp/MukFlap/Reply03/DataModel/Contracts/IMukFlapAirReply03Telemetry.cs
@@ -0,0 +1,24 @@
+﻿using CustomModbusSlave.Es2gClimatic.Shared.MukFlap.Diagnostic2;
+using CustomModbusSlave.Es2gClimatic.Shared.MukFlap.DiagnosticOneWire;
+using CustomModbusSlave.Es2gClimatic.Shared.SensorIndications;
+
+namespace CustomModbusSlave.Es2gClimatic.CabinApp.MukFlap.Reply03.DataModel.Contracts {
+	internal interface IMukFlapAirReply03Telemetry {
+		int FlapPosition { get; }
+		ISensorIndication<double> TemperatureAddress1 { get; }
+		ISensorIndication<double> TemperatureAddress2 { get; }
+		IIncomingSignals IncomingSignals { get; }
+		byte OutgoingSignals { get; }
+		double AnalogInput { get; }
+		IMukFlapWorkmodeStage AutomaticModeStage { get; }
+		IMukFlapDiagnostic1 Diagnostic1 { get; }
+		IMukFlapDiagnostic2 Diagnostic2 { get; }
+		IMukFlapDiagnosticOneWireSensor Diagnostic3OneWire1 { get; }
+		IMukFlapDiagnosticOneWireSensor Diagnostic4OneWire2 { get; }
+		IEmersonDiagnostic EmersonDiagnostic { get; }
+		ISensorIndication<double> EmersonTemperature { get; }
+		ISensorIndication<double> EmersonPressure { get; }
+		int EmersonValveSetting { get; }
+		int FirmwareBuildNumber { get; }
+	}
+}
