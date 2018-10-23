f96364b9dce2d31a0c1cb096d8d74191d9545e7b
Alexey Debelov <alienjustmail@gmail.com>
2018-10-23 08:24:41 +0500

    Search comparison improved

--- source/CustomModbusSlave.Es2gClimatic.Shared/Search/SearchViewModel.cs
+++ source/CustomModbusSlave.Es2gClimatic.Shared/Search/SearchViewModel.cs
@@ -46,7 +46,7 @@ namespace CustomModbusSlave.Es2gClimatic.Shared.Search {
 			else {
 				foreach (var topLevelGroup in _rootGroups) {
 					int paramsCount = 0;
-					var copyOfDispGroup = FilterGroupsAndParamsRecurse(topLevelGroup, _filterText.ToLower(CultureInfo.CurrentCulture), new List<string>(), ref paramsCount);
+					var copyOfDispGroup = FilterGroupsAndParamsRecurse(topLevelGroup, new List<string>(), ref paramsCount);
 					if (copyOfDispGroup != null) ResultParamsVm.AddParameterOrGroup(copyOfDispGroup);
 				}
 				
@@ -54,20 +54,20 @@ namespace CustomModbusSlave.Es2gClimatic.Shared.Search {
 			}
 		}
 
-		private IDisplayGroup FilterGroupsAndParamsRecurse(IDisplayGroup dispGroup, string filterText, IEnumerable<string> parentNames, ref int paramsCount) {
+		private IDisplayGroup FilterGroupsAndParamsRecurse(IDisplayGroup dispGroup, IEnumerable<string> parentNames, ref int paramsCount) {
 			List<string> searchTags = new List<string>(parentNames) {dispGroup.DisplayName};
 
 			int subParamsCount = paramsCount;
 			var copyOfDispGroup = new GroupParamViewModel(dispGroup.DisplayName);
 			foreach (var param in dispGroup.ParametersAndGroups) {
 				if (param is IDisplayGroup group) {
-					var copyOfSubgroup = FilterGroupsAndParamsRecurse(group, filterText, searchTags, ref paramsCount);
+					var copyOfSubgroup = FilterGroupsAndParamsRecurse(group, searchTags, ref paramsCount);
 					if (copyOfSubgroup != null) {
 						copyOfDispGroup.AddParameterOrGroup(copyOfSubgroup);
 					}
 				}
 				else {
-					if (searchTags.Any(tag => tag.ToLower(CultureInfo.CurrentCulture).Contains(_filterText)) || param.DisplayName.ToLower(CultureInfo.CurrentCulture).Contains(_filterText)) {
+					if (searchTags.Any(tag => tag.IndexOf(_filterText, StringComparison.OrdinalIgnoreCase) >= 0) || param.DisplayName.IndexOf(_filterText, StringComparison.OrdinalIgnoreCase) >= 0) {
 						copyOfDispGroup.AddParameterOrGroup(param);
 						paramsCount++;
 					}
