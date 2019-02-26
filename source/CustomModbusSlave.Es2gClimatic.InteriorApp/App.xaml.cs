a393b3aa8199037c7178b9f351459305aefbcaa3
Alexey Debelov <alienjustmail@gmail.com>
2019-02-26 08:17:53 +0500

    Fixed build

--- source/CustomModbusSlave.Es2gClimatic.InteriorApp/App.xaml.cs
+++ source/CustomModbusSlave.Es2gClimatic.InteriorApp/App.xaml.cs
@@ -19,8 +19,6 @@ using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFanAirExhauster.View;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFanAirExhauster.ViewModel;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirOuter;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirOuter.Reply03;
-using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirOuter.Reply03.DataModel;
-using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirOuter.Reply03.DataModel.Contracts;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirOuter.Request16;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirRecycle;
 using CustomModbusSlave.Es2gClimatic.InteriorApp.MukFlapAirRecycle.Reply03;
@@ -41,25 +39,29 @@ using CustomModbusSlave.Es2gClimatic.Shared.MukFanCondenser.Request16;
 using CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator;
 using CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator.Reply03;
 using CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator.Request16;
-using CustomModbusSlave.Es2gClimatic.Shared.OneWire;
-using CustomModbusSlave.Es2gClimatic.Shared.Oscilloscope;
 using CustomModbusSlave.Es2gClimatic.Shared.Search;
 using CustomModbusSlave.Es2gClimatic.Shared.SetParamsAndKsm;
 using CustomModbusSlave.Es2gClimatic.Shared.TestSystems;
 using CustomModbusSlave.Es2gClimatic.Shared.UniversalParams.Vm;
 using Timer = System.Timers.Timer;
 
-namespace CustomModbusSlave.Es2gClimatic.InteriorApp {
+namespace CustomModbusSlave.Es2gClimatic.InteriorApp
+{
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application {
-		private void App_OnStartup(object sender, StartupEventArgs e) {
-			IWorker<Action> sharedTestWorker = new SingleThreadedRelayQueueWorkerProceedAllItemsBeforeStopNoLog<Action>("Shared Test Worker", a => {
-				try {
+	public partial class App : Application
+	{
+		private void App_OnStartup(object sender, StartupEventArgs e)
+		{
+			IWorker<Action> sharedTestWorker = new SingleThreadedRelayQueueWorkerProceedAllItemsBeforeStopNoLog<Action>("Shared Test Worker", a =>
+			{
+				try
+				{
 					a();
 				}
-				catch (Exception exception) {
+				catch (Exception exception)
+				{
 					Console.WriteLine(exception);
 				}
 			}, ThreadPriority.BelowNormal, true, ApartmentState.Unknown);
@@ -69,33 +71,33 @@ namespace CustomModbusSlave.Es2gClimatic.InteriorApp {
 
 
 			var colorsForGraphics = new List<Color> {
-				Colors.LawnGreen,
-				Colors.Red,
-				Colors.Cyan,
-				Colors.Yellow,
-				Colors.Coral,
-				Colors.LightGreen,
-				Colors.HotPink,
-				Colors.DeepSkyBlue,
-				Colors.Gold,
-				Colors.Orange,
-				Colors.Violet,
-				Colors.White,
-				Colors.Fuchsia,
-				Colors.LightSkyBlue,
-				Colors.LightGray,
-				Colors.Khaki,
-				Colors.SpringGreen,
-				Colors.Tomato,
-				Colors.LightCyan,
-				Colors.Goldenrod,
-				Colors.SlateBlue,
-				Colors.Cornsilk,
-				Colors.MediumPurple,
-				Colors.RoyalBlue,
-				Colors.MediumVioletRed,
-				Colors.MediumTurquoise
-			};
+								Colors.LawnGreen,
+								Colors.Red,
+								Colors.Cyan,
+								Colors.Yellow,
+								Colors.Coral,
+								Colors.LightGreen,
+								Colors.HotPink,
+								Colors.DeepSkyBlue,
+								Colors.Gold,
+								Colors.Orange,
+								Colors.Violet,
+								Colors.White,
+								Colors.Fuchsia,
+								Colors.LightSkyBlue,
+								Colors.LightGray,
+								Colors.Khaki,
+								Colors.SpringGreen,
+								Colors.Tomato,
+								Colors.LightCyan,
+								Colors.Goldenrod,
+								Colors.SlateBlue,
+								Colors.Cornsilk,
+								Colors.MediumPurple,
+								Colors.RoyalBlue,
+								Colors.MediumVioletRed,
+								Colors.MediumTurquoise
+						};
 
 
 			var appFactory = new AppFactory("psn.S2G-climatic-interior.xml");
@@ -156,42 +158,47 @@ namespace CustomModbusSlave.Es2gClimatic.InteriorApp {
 			appAbilities.CmdNotifierStd.AddListener(cmdListenerWinSum);
 
 
-			if (appAbilities.Version == AppVersion.Full) {
-				appFactory.ShowChildWindowInOwnThread(uiNotifier => {
+			if (appAbilities.Version == AppVersion.Full)
+			{
+				appFactory.ShowChildWindowInOwnThread(uiNotifier =>
+				{
 					var chartVm = new ChartViewModel(uiNotifier, colorsForGraphics);
 					appAbilities.ParamLoggerRegistrationPoint.RegisterLoggegr(chartVm); // TODO: REG on point
-					var chartWindow = new WindowChart();
+									var chartWindow = new WindowChart();
 					chartVm.SetUpdatable(chartWindow);
 
 					return new WindowAndClosableViewModel(chartWindow, new WindowChartViewModel(chartVm));
 				});
 			}
 
-			appFactory.ShowMainWindowInOwnThread("Технический абонент, салон", appAbilities, mainVm => {
+			appFactory.ShowMainWindowInOwnThread("Технический абонент, салон", appAbilities, mainVm =>
+			{
 				var channel = mainVm.AddChannel("Single channel");
 				mainVm.TopContent = new TopContentView { DataContext = new TopContentViewModel(mainVm.Notifier, cmdListenerBsSmReply32) };
-				
+
 				var searchVm = new SearchViewModel(mainVm.Notifier); // Search View Model inited first to add items in future
-				
-				var tabsBuilderSd = new SdTabInterfaceBuilder(mainVm.Notifier, cmdListenerKsmParams, appAbilities.ParameterLogger, channel.Channel.ParamSetter, appAbilities.Version);
+
+							var tabsBuilderSd = new SdTabInterfaceBuilder(mainVm.Notifier, cmdListenerKsmParams, appAbilities.ParameterLogger, channel.Channel.ParamSetter, appAbilities.Version);
 				var tabVmSd = tabsBuilderSd.Build();
-				
-				var tabsBuilderM3 = new TabInterfaceBuilderMuk3(mainVm.Notifier,cmdListenerMukVaporizerReply03, cmdListenerMukVaporizerRequest16, appAbilities.ParameterLogger, channel.Channel.ParamSetter);
+
+				var tabsBuilderM3 = new TabInterfaceBuilderMuk3(mainVm.Notifier, cmdListenerMukVaporizerReply03, cmdListenerMukVaporizerRequest16, appAbilities.ParameterLogger, channel.Channel.ParamSetter);
 				var tabVmMuk3 = tabsBuilderM3.Build();
 				searchVm.RegisterTopLevelGroup(tabVmMuk3);
-				
-				
+
+
 				mainVm.AddTab(new TabItemViewModel { FullHeader = "Диагностика системы", ShortHeader = "ДС", Content = new SystemDiagnosticView { DataContext = new SystemDiagnosticViewModel(appAbilities.Version == AppVersion.Full, appAbilities.Version == AppVersion.Half || appAbilities.Version == AppVersion.Full, appAbilities.IsHourCountersVisible, mainVm.Notifier, cmdListenerMukFlapOuterAirReply03, cmdListenerMukVaporizerReply03, cmdListenerMukVaporizerRequest16, cmdListenerMukCondenserFanReply03, cmdListenerMukAirExhausterReply03, cmdListenerMukFlapReturnAirReply03, cmdListenerMukFlapWinterSummerReply03, cmdListenerBsSmRequest32, cmdListenerBsSmReply32, cmdListenerKsmParams, cmdListenerBvs1Reply65, cmdListenerBvs2Reply65, tabVmSd) } });
 
-				if (appAbilities.Version == AppVersion.Full) {
+				if (appAbilities.Version == AppVersion.Full)
+				{
 
 
-					mainVm.AddTab(new TabItemViewModel {FullHeader = "Тестирование системы", ShortHeader = "ТЕСТ", Content = new TestSystemView {DataContext = new TestSystemViewModel(mainVm.Notifier, "Охлаждение 100%", new Freeze100Test(sharedTestWorker, cmdListenerMukVaporizerReply03, tt, channel.Channel.ParamSetter), AlienJust.Adaptation.WindowsPresentation.Converters.Colors.Transparent)}});
+					mainVm.AddTab(new TabItemViewModel { FullHeader = "Тестирование системы", ShortHeader = "ТЕСТ", Content = new TestSystemView { DataContext = new TestSystemViewModel(mainVm.Notifier, "Охлаждение 100%", new Freeze100Test(sharedTestWorker, cmdListenerMukVaporizerReply03, cmdListenerMukFlapWinterSummerReply03, tt, channel.Channel.ParamSetter), AlienJust.Adaptation.WindowsPresentation.Converters.Colors.Transparent) } });
 				}
 
 
-				if (appAbilities.Version == AppVersion.Full) {
-					mainVm.AddTab(new TabItemViewModel {FullHeader = "Глобальный поиск параметров", ShortHeader = "ПОИСК", Content = new SearchView {DataContext = searchVm}});
+				if (appAbilities.Version == AppVersion.Full)
+				{
+					mainVm.AddTab(new TabItemViewModel { FullHeader = "Глобальный поиск параметров", ShortHeader = "ПОИСК", Content = new SearchView { DataContext = searchVm } });
 				}
 
 				if (appAbilities.Version == AppVersion.Full)
@@ -212,24 +219,28 @@ namespace CustomModbusSlave.Es2gClimatic.InteriorApp {
 				if (appAbilities.Version == AppVersion.Full)
 					mainVm.AddTab(new TabItemViewModel { FullHeader = "МУК заслонки лето зима", ShortHeader = "МУК 8", Content = new MukFlapWinterSummerDataView { DataContext = new MukFlapWinterSummerViewModel(mainVm.Notifier, channel.Channel.ParamSetter, cmdListenerMukFlapWinterSummerReply03, cmdListenerMukAirFlapWinterSummerRequest16) } });
 
-				if (appAbilities.Version == AppVersion.Full) {
+				if (appAbilities.Version == AppVersion.Full)
+				{
 					var tabsBuilderMuk8 = new MukFlapAirWinterSummer.TabInterfaceBuilder(mainVm.Notifier, cmdListenerWinSum, cmdListenerMukAirFlapWinterSummerRequest16, appAbilities.ParameterLogger, channel.Channel.ParamSetter);
 					var tabVmMuk8 = tabsBuilderMuk8.Build();
 					searchVm.RegisterTopLevelGroup(tabVmMuk8);
-					mainVm.AddTab(new TabItemViewModel {FullHeader = "МУК заслонки лето зима", ShortHeader = "МУК 8", Content = new ParametersListView {DataContext = tabVmMuk8}});
+					mainVm.AddTab(new TabItemViewModel { FullHeader = "МУК заслонки лето зима", ShortHeader = "МУК 8", Content = new ParametersListView { DataContext = tabVmMuk8 } });
 				}
-				/*
-				if (appAbilities.Version == AppVersion.Full) {
-					var tabsBuilderKsm = new Ksm.KsmTabInterfaceBuilder(mainVm.Notifier, cmdListenerKsmParams, appAbilities.ParameterLogger, channel.Channel.ParamSetter, appAbilities.Version);
-					var tabVmKsm = tabsBuilderKsm.Build();
-					searchVm.RegisterTopLevelGroup(tabVmKsm);
-					mainVm.AddTab(new TabItemViewModel {FullHeader = "КСМ и в Африке КСМ", ShortHeader = "КСМ", Content = new ParametersListView {DataContext = tabVmKsm}});
-				}
-				*/
+							/*
+			if (appAbilities.Version == AppVersion.Full) {
+				var tabsBuilderKsm = new Ksm.KsmTabInterfaceBuilder(mainVm.Notifier, cmdListenerKsmParams, appAbilities.ParameterLogger, channel.Channel.ParamSetter, appAbilities.Version);
+				var tabVmKsm = tabsBuilderKsm.Build();
+				searchVm.RegisterTopLevelGroup(tabVmKsm);
+				mainVm.AddTab(new TabItemViewModel {FullHeader = "КСМ и в Африке КСМ", ShortHeader = "КСМ", Content = new ParametersListView {DataContext = tabVmKsm}});
+			}
+			*/
 
 				if (appAbilities.Version == AppVersion.Full || appAbilities.Version == AppVersion.Half)
-					mainVm.AddTab(new TabItemViewModel {
-						FullHeader = "БС-СМ", ShortHeader = "БС-СМ", Content = new BsSmDataView { DataContext = new BsSmDataViewModel(mainVm.Notifier, cmdListenerBsSmRequest32, cmdListenerBsSmReply32) }
+					mainVm.AddTab(new TabItemViewModel
+					{
+						FullHeader = "БС-СМ",
+						ShortHeader = "БС-СМ",
+						Content = new BsSmDataView { DataContext = new BsSmDataViewModel(mainVm.Notifier, cmdListenerBsSmRequest32, cmdListenerBsSmReply32) }
 					});
 
 				if (appAbilities.Version == AppVersion.Full || appAbilities.Version == AppVersion.Half)
--- source/CustomModbusSlave.Es2gClimatic.InteriorApp/TestSys/Freeze100Test.cs
+++ source/CustomModbusSlave.Es2gClimatic.InteriorApp/TestSys/Freeze100Test.cs
@@ -8,164 +8,182 @@ using CustomModbusSlave.Es2gClimatic.Shared;
 using CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator.Reply03;
 using CustomModbusSlave.Es2gClimatic.Shared.TestSystems;
 
-namespace CustomModbusSlave.Es2gClimatic.InteriorApp.TestSys {
-	internal class Freeze100Test : ITestSysAsync {
-		private readonly IWorker<Action> _sharedTestsWorker;
-		private readonly ICmdListener<IMukFanVaporizerDataReply03> _evaporator03Listener;
-		private readonly Timer _sharedTestTimer;
-		private readonly IParameterSetter _parameterSetter;
+namespace CustomModbusSlave.Es2gClimatic.InteriorApp.TestSys
+{
+    internal class Freeze100Test : ITestSysAsync
+    {
+        private readonly IWorker<Action> _sharedTestsWorker;
+        private readonly ICmdListener<IMukFanVaporizerDataReply03> _evaporator03Listener;
+        private readonly ICmdListener<IMukFlapWinterSummerReply03Telemetry> _mukFlapWinterSummerReply03Listener;
+        private readonly Timer _sharedTestTimer;
+        private readonly IParameterSetter _parameterSetter;
 
-		private readonly object _isTestRunningSync;
-		private bool _isTestRunning;
+        private readonly object _isTestRunningSync;
+        private bool _isTestRunning;
 
-		private readonly object _isTestCanceledSync;
-		private bool _isTestCanceled;
+        private readonly object _isTestCanceledSync;
+        private bool _isTestCanceled;
 
-		private readonly object _lastReceivedDataSyncMukFanVaporizer03Reply;
-		private IMukFanVaporizerDataReply03 _lastReceivedDataMukFanVaporizer03Reply;
-		
-		private readonly object _lastReceivedDataSyncMukFFlapWinterSummer03Reply;
-		private IMukFanVaporizerDataReply03 _lastReceivedDataMukFanVaporizer03Reply;
+        private readonly object _lastReceivedDataSyncMukFanVaporizer03Reply;
+        private IMukFanVaporizerDataReply03 _lastReceivedDataMukFanVaporizer03Reply;
 
-		private readonly object _timeSync;
-		private DateTime _beginTime;
+        private readonly object _lastReceivedDataSyncMukFlapWinterSummer03Reply;
+        private IMukFlapWinterSummerReply03Telemetry _lastReceivedDataMukFlapWinterSummer03Reply;
 
-		private readonly TimeSpan _testTimeout;
+        private readonly object _timeSync;
+        private DateTime _beginTime;
 
-		private readonly object _testCompleteSync;
-		private Action<TestSysResult> _testComplete;
+        private readonly TimeSpan _testTimeout;
 
-		private readonly object _progressChangedSync;
-		private Action<double, TestSysStepResult, string> _progressChanged;
+        private readonly object _testCompleteSync;
+        private Action<TestSysResult> _testComplete;
 
-		public Freeze100Test(IWorker<Action> sharedTestsWorker, ICmdListener<IMukFanVaporizerDataReply03> evaporator03Listener, ICmdListener<IMukFlapWinterSummerReply03Telemetry> mukFlapWinterSummerReply03Listener, Timer sharedTestTimer, IParameterSetter parameterSetter) {
-			_sharedTestsWorker = sharedTestsWorker;
-			_evaporator03Listener = evaporator03Listener;
-			_sharedTestTimer = sharedTestTimer;
-			_parameterSetter = parameterSetter;
+        private readonly object _progressChangedSync;
+        private Action<double, TestSysStepResult, string> _progressChanged;
 
-			_sharedTestTimer.Elapsed += SharedTestTimerOnElapsed;
+        public Freeze100Test(IWorker<Action> sharedTestsWorker, ICmdListener<IMukFanVaporizerDataReply03> evaporator03Listener, ICmdListener<IMukFlapWinterSummerReply03Telemetry> mukFlapWinterSummerReply03Listener, Timer sharedTestTimer, IParameterSetter parameterSetter)
+        {
+            _sharedTestsWorker = sharedTestsWorker;
 
-			_evaporator03Listener.DataReceived += Evaporator03ListenerOnDataReceived;
+            _evaporator03Listener = evaporator03Listener;
+            _mukFlapWinterSummerReply03Listener = mukFlapWinterSummerReply03Listener;
 
-			_isTestCanceledSync = new object();
-			_isTestCanceled = false;
+            _sharedTestTimer = sharedTestTimer;
+            _parameterSetter = parameterSetter;
 
-			_isTestRunningSync = new object();
-			_isTestRunning = false;
+            _sharedTestTimer.Elapsed += SharedTestTimerOnElapsed;
 
-			_timeSync = new object();
-			_testTimeout = TimeSpan.FromSeconds(5.0);
+            _evaporator03Listener.DataReceived += Evaporator03ListenerOnDataReceived;
 
-			_lastReceivedDataSyncMukFanVaporizer03Reply = new object();
-			_lastReceivedDataMukFanVaporizer03Reply = null;
+            _isTestCanceledSync = new object();
+            _isTestCanceled = false;
 
-			_testCompleteSync = new object();
-			_testComplete = null;
+            _isTestRunningSync = new object();
+            _isTestRunning = false;
 
-			_progressChangedSync = new object();
-			_progressChanged = null;
-		}
+            _timeSync = new object();
+            _testTimeout = TimeSpan.FromSeconds(5.0);
 
-		public DateTime BeginTime {
-			get {
-				lock (_timeSync) return _beginTime;
-			}
-			set {
-				lock (_timeSync) _beginTime = value;
-			}
-		}
+            _lastReceivedDataSyncMukFanVaporizer03Reply = new object();
+            _lastReceivedDataMukFanVaporizer03Reply = null;
 
-		public void BeginTest(Action testHasBegun, Action<double, TestSysStepResult, string> progressChanged, Action<TestSysResult> testComplete) {
-			_sharedTestsWorker.AddWork(() => {
-				testHasBegun.Invoke();
-				BeginTime = DateTime.Now;
-				ProgressChanged = progressChanged;
-				TestComplete = testComplete;
-				IsTestRunning = true;
+            _testCompleteSync = new object();
+            _testComplete = null;
 
-				_parameterSetter.SetParameterAsync(48, 1, Console.WriteLine);
-				ProgressChanged.Invoke(100, TestSysStepResult.Good, "48 параметр КСМ установлен в режим Охлаждение 100%");
-			});
-		}
+            _progressChangedSync = new object();
+            _progressChanged = null;
+        }
 
-		private void SharedTestTimerOnElapsed(object sender, ElapsedEventArgs e) {
-			if (IsTestRunning) {
-				if (IsTestCanceled) {
-					IsTestRunning = false;
-					IsTestCanceled = true;
-					TestComplete.Invoke(TestSysResult.Canceled);
-				}
-				else if (e.SignalTime - BeginTime > _testTimeout) {
-					// timeout
-					IsTestRunning = false;
-					ProgressChanged.Invoke(100, TestSysStepResult.Bad, "МУК не включает Emerson");
-					TestComplete.Invoke(TestSysResult.Fail);
-				}
-				else {
-					if (LastReceivedData.Diagnostic3Parsed.MukIsSwitchingEmersionOn) {
-						IsTestRunning = false;
-						LastReceivedData = null;
-						ProgressChanged.Invoke(100, TestSysStepResult.Good, "Включение Emerson осуществлено");
-						TestComplete.Invoke(TestSysResult.Success);
-					}
-				}
-			}
-		}
+        public DateTime BeginTime {
+            get {
+                lock (_timeSync) return _beginTime;
+            }
+            set {
+                lock (_timeSync) _beginTime = value;
+            }
+        }
 
-		public Action<double, TestSysStepResult, string> ProgressChanged {
-			get {
-				lock (_progressChangedSync) return _progressChanged;
-			}
-			set {
-				lock (_progressChangedSync) _progressChanged = value;
-			}
-		}
+        public void BeginTest(Action testHasBegun, Action<double, TestSysStepResult, string> progressChanged, Action<TestSysResult> testComplete)
+        {
+            _sharedTestsWorker.AddWork(() =>
+            {
+                testHasBegun.Invoke();
+                BeginTime = DateTime.Now;
+                ProgressChanged = progressChanged;
+                TestComplete = testComplete;
+                IsTestRunning = true;
 
-		public Action<TestSysResult> TestComplete {
-			get {
-				lock (_testCompleteSync) return _testComplete;
-			}
-			set {
-				lock (_testCompleteSync) _testComplete = value;
-			}
-		}
+                _parameterSetter.SetParameterAsync(48, 1, Console.WriteLine);
+                ProgressChanged.Invoke(100, TestSysStepResult.Good, "48 параметр КСМ установлен в режим Охлаждение 100%");
+            });
+        }
 
-		private void Evaporator03ListenerOnDataReceived(IList<byte> bytes, IMukFanVaporizerDataReply03 data) {
-			if (!IsTestCanceled) {
-				lock (_lastReceivedDataSyncMukFanVaporizer03Reply) _lastReceivedDataMukFanVaporizer03Reply = data;
-			}
-		}
+        private void SharedTestTimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (IsTestRunning)
+            {
+                if (IsTestCanceled)
+                {
+                    IsTestRunning = false;
+                    IsTestCanceled = true;
+                    TestComplete.Invoke(TestSysResult.Canceled);
+                }
+                else if (e.SignalTime - BeginTime > _testTimeout)
+                {
+                    // timeout
+                    IsTestRunning = false;
+                    ProgressChanged.Invoke(100, TestSysStepResult.Bad, "МУК не включает Emerson");
+                    TestComplete.Invoke(TestSysResult.Fail);
+                }
+                else
+                {
+                    if (LastReceivedData.Diagnostic3Parsed.MukIsSwitchingEmersionOn)
+                    {
+                        IsTestRunning = false;
+                        LastReceivedData = null;
+                        ProgressChanged.Invoke(100, TestSysStepResult.Good, "Включение Emerson осуществлено");
+                        TestComplete.Invoke(TestSysResult.Success);
+                    }
+                }
+            }
+        }
 
-		public void CancelCurrentTest() {
-			lock (_isTestCanceledSync) _isTestCanceled = true;
-		}
+        public Action<double, TestSysStepResult, string> ProgressChanged {
+            get {
+                lock (_progressChangedSync) return _progressChanged;
+            }
+            set {
+                lock (_progressChangedSync) _progressChanged = value;
+            }
+        }
 
-		public bool IsTestCanceled {
-			get {
-				lock (_isTestCanceledSync) return _isTestCanceled;
-			}
-			set {
-				lock (_isTestCanceledSync) _isTestCanceled = value;
-			}
-		}
+        public Action<TestSysResult> TestComplete {
+            get {
+                lock (_testCompleteSync) return _testComplete;
+            }
+            set {
+                lock (_testCompleteSync) _testComplete = value;
+            }
+        }
 
-		public bool IsTestRunning {
-			get {
-				lock (_isTestRunningSync) return _isTestRunning;
-			}
-			set {
-				lock (_isTestRunningSync) _isTestRunning = value;
-			}
-		}
+        private void Evaporator03ListenerOnDataReceived(IList<byte> bytes, IMukFanVaporizerDataReply03 data)
+        {
+            if (!IsTestCanceled)
+            {
+                lock (_lastReceivedDataSyncMukFanVaporizer03Reply) _lastReceivedDataMukFanVaporizer03Reply = data;
+            }
+        }
 
-		public IMukFanVaporizerDataReply03 LastReceivedData {
-			get {
-				lock (_lastReceivedDataSyncMukFanVaporizer03Reply) return _lastReceivedDataMukFanVaporizer03Reply;
-			}
-			set {
-				lock (_lastReceivedDataSyncMukFanVaporizer03Reply) _lastReceivedDataMukFanVaporizer03Reply = value;
-			}
-		}
-	}
+        public void CancelCurrentTest()
+        {
+            lock (_isTestCanceledSync) _isTestCanceled = true;
+        }
+
+        public bool IsTestCanceled {
+            get {
+                lock (_isTestCanceledSync) return _isTestCanceled;
+            }
+            set {
+                lock (_isTestCanceledSync) _isTestCanceled = value;
+            }
+        }
+
+        public bool IsTestRunning {
+            get {
+                lock (_isTestRunningSync) return _isTestRunning;
+            }
+            set {
+                lock (_isTestRunningSync) _isTestRunning = value;
+            }
+        }
+
+        public IMukFanVaporizerDataReply03 LastReceivedData {
+            get {
+                lock (_lastReceivedDataSyncMukFanVaporizer03Reply) return _lastReceivedDataMukFanVaporizer03Reply;
+            }
+            set {
+                lock (_lastReceivedDataSyncMukFanVaporizer03Reply) _lastReceivedDataMukFanVaporizer03Reply = value;
+            }
+        }
+    }
 }
