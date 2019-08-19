f155bade54b8f022ae08be808199373c1e1ef904
Alexey Debelov <alienjustmail@gmail.com>
2019-08-19 14:40:32 +0500

    Code for MUK evaporator fan was copied to each project that used it cause it became differ for each project

--- source/CustomModbusSlave.Climatic.Shared/MukFanEvaporator/Reply03/IMukFanVaporizerDataReply03Diagnostic1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomModbusSlave.Es2gClimatic.Shared.MukFanEvaporator.Reply03 {
-	public interface IMukFanVaporizerDataReply03Diagnostic1 {
-		bool FanControllerLinkLost { get; }
-		bool FanErrorByDiscreeteInput { get; }
-		bool ErrorOneWireSensor1 { get; }
-		bool ErrorOneWireSensor2 { get; }
-	}
-}
