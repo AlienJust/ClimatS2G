25dd23203e8c89c63826208f943c4a0e284e2526
Alexey Debelov <alienjustmail@gmail.com>
2020-09-21 14:28:23 +0500

    Added events for parameters

---
+++ source/CustomModbusSlave.Climatic.Shared/ParameterPresentation/Events/BooleanView.cs
@@ -0,0 +1,25 @@
+﻿using NCalc;
+
+namespace CustomModbusSlave.Es2gClimatic.Shared.ParameterPresentation
+{
+    public sealed class BooleanEvent : IParameterEvent
+    {
+        private readonly string _expression;
+
+        public string Name { get; }
+
+        public BooleanEvent(string expression, string name)
+        {
+            _expression = expression;
+            Name = name;
+        }
+
+        bool CheckForEvent(double value)
+        {
+            var expr = new Expression(_expression);
+            expr.Parameters.Add("value", value);
+            bool result = (bool)expr.Evaluate();
+            return result;
+        }
+    }
+}
--- source/CustomModbusSlave.Climatic.Shared/ParameterPresentation/IParameterDescription.cs
+++ source/CustomModbusSlave.Climatic.Shared/ParameterPresentation/IParameterDescription.cs
@@ -1,4 +1,5 @@
-﻿namespace CustomModbusSlave.Es2gClimatic.Shared.ParameterPresentation
+﻿using System.Collections.Generic;
+namespace CustomModbusSlave.Es2gClimatic.Shared.ParameterPresentation
 {
     public interface IParameterDescription
     {
@@ -9,6 +10,14 @@
 
         IParameterView View { get; }
 
+        IList<IParameterEvent> Events { get; }
+
         IParameterInjectionConfiguration Injection { get; }
+    }
+
+    public interface IParameterEvent
+    {
+        string Name { get; }
+        bool CheckForEvent(double value);
     }
 }
