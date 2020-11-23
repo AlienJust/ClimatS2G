9ff22452aa5628ff5b024db82d21f32c2728cd12
Alexey Debelov <alienjustmail@gmail.com>
2020-11-23 18:22:11 +0500

    Created project for TEM31 cabin

---
+++ source/CustomModbusSlave.Climatic.Tem31CabinApp/MainContentView.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CustomModbusSlave.Climatic.TramHorizonInteriorApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainContent.xaml
+    /// </summary>
+    public partial class MainContentView : UserControl
+    {
+        public MainContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ source/CustomModbusSlave.Climatic.Tem31CabinApp/ParameterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CustomModbusSlave.Climatic.TramHorizonInteriorApp
+{
+    /// <summary>
+    /// Interaction logic for ParameterView.xaml
+    /// </summary>
+    public partial class ParameterView : UserControl
+    {
+        public ParameterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
